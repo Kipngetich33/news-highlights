7f05d63d7137291083c6b9e4795278c4f599b547
Kipngetich33 <khalifngeno@gmail.com>
2017-12-08 05:06:16 -0500

    add initial files and folders

---
+++ app/models.py
@@ -0,0 +1,20 @@
+class News_Highlight:
+    '''
+    This is class that will determine the behaviour of the news clas
+    '''
+    def __init__(self,source_names,image_url,description):
+        '''
+        function instantiates the class sources 
+        '''
+        self.source_names = source_names
+        self.image_url = image_url
+        self.description =description
+         
+        
+
+    # def display_news(self):
+    #     '''
+    #     This function will display soureces in the index page
+    #     '''
+
+
